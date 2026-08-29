7cf68c040923f4f81b23370cc6807e14293a7396
nimboxa-maker <nimboxa@gmail.com>
2026-08-28 17:03:31 -0700

    feat: use extended database types

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,5 +1,5 @@
 import { createBrowserClient } from "@supabase/ssr";
-import type { Database } from "@/lib/supabase/database.types";
+import type { Database } from "@/lib/supabase/database";
 
 export function createClient() {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
