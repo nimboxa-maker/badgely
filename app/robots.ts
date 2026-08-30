5d923e44d529669d087f825dc86ffb29746b530a
nimboxa-maker <nimboxa@gmail.com>
2026-08-29 20:03:12 -0700

    feat: add robots directives

---
+++ app/robots.ts
@@ -0,0 +1,21 @@
+import type { MetadataRoute } from "next";
+
+const siteUrl = (process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000").replace(/\/$/, "");
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: [
+        "/admin/",
+        "/auth/",
+        "/dashboard",
+        "/sign-in",
+        "/sign-up",
+        "/study-plans/",
+      ],
+    },
+    sitemap: `${siteUrl}/sitemap.xml`,
+  };
+}
