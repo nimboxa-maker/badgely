6ae2c58b9f4b61fbf9baf0dcf1af98e732732ae2
nimboxa-maker <nimboxa@gmail.com>
2026-09-11 13:10:23 -0700

    Fix robots sitemap URL

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 
-const siteUrl = "https://AimToCert.com";
+const siteUrl = (
+  process.env.NEXT_PUBLIC_SITE_URL ?? "https://www.aimtocert.com"
+).replace(/\/$/, "");
 
 export default function robots(): MetadataRoute.Robots {
   return {
